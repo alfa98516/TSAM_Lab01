4c1ed47008030930b63abb262de039a087d5e124
gislih24 <gislih24@ru.is>
2026-08-22 15:39:57 +0000

    Format client.cpp

--- client.cpp
+++ client.cpp
@@ -1,20 +1,19 @@
 
 #include <arpa/inet.h>
+#include <cstdlib>
+#include <iostream>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <iostream>
-#include <sys/socket.h>
-#include <cstdlib>
 #include <string>
+#include <sys/socket.h>
 
 int main(int argc, const char* argv[]) {
-    
 
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <IPv4 address>" << std::endl;
         return EXIT_FAILURE;
     }
-    const char *ipaddr = argv[1];
+    const char* ipaddr = argv[1];
 
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -26,14 +25,17 @@ int main(int argc, const char* argv[]) {
 
     struct sockaddr_in destaddr;
     destaddr.sin_family = AF_INET;
-    destaddr.sin_port = htons(4001); // TODO: take port number from command line arguments
+    destaddr.sin_port =
+        htons(4001); // TODO: take port number from command line arguments
     if ((inet_pton(AF_INET, ipaddr, &destaddr.sin_addr)) < 1) {
-        std::cerr << "invalid ip address or address family: " << ipaddr << std::endl;
+        std::cerr << "invalid ip address or address family: " << ipaddr
+                  << std::endl;
         exit(1);
     }
 
     int ret;
-    if ((ret = (sendto(sockfd, &s, sizeof(s), 0 /*flags*/, (struct sockaddr*)&destaddr, sizeof(destaddr)))) < 0) {
+    if ((ret = (sendto(sockfd, &s, sizeof(s), 0 /*flags*/,
+                       (struct sockaddr*)&destaddr, sizeof(destaddr)))) < 0) {
         perror("Error sending");
         exit(1);
     }
@@ -42,7 +44,8 @@ int main(int argc, const char* argv[]) {
     socklen_t srcaddrlen;
     char buffer[2048];
 
-    if ((ret = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&srcaddr, &srcaddrlen)) < 0) {
+    if ((ret = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+                        (struct sockaddr*)&srcaddr, &srcaddrlen)) < 0) {
         perror("Error recieving");
         exit(1);
     }
