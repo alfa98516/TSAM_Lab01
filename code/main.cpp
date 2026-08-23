bcdf1fd10bfdf9bfdbe47c9b3a20d69c4d95ede1
gislih24 <gislih24@ru.is>
2026-08-23 11:58:38 +0000

    Add close() statement and explanatory comments

--- code/main.cpp
+++ code/main.cpp
@@ -5,9 +5,11 @@
 #include <stdio.h>
 #include <string>
 #include <sys/socket.h>
+#include <unistd.h>
 
 int main(int argc, const char* argv[]) {
     const char* ip_addr = argv[1]; // TODO: check for bounds
+    // Try to make an IPv4 UDP socket.
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (socket_fd < 0) {
         perror("Error creating socket!");
@@ -19,6 +21,7 @@ int main(int argc, const char* argv[]) {
 
     struct sockaddr_in dest_addr;
 
+    // Set address family to IPv4, and port number to 4006.
     dest_addr.sin_family = AF_INET;
     dest_addr.sin_port =
         htons(4006); // HACK: take port number from  command line argument.
@@ -27,6 +30,7 @@ int main(int argc, const char* argv[]) {
         exit(1);
     }
 
+    // Send the string to the destination address.
     ssize_t ret = sendto(socket_fd, s.c_str(), s.length(), 0,
                          (struct sockaddr*)&dest_addr, sizeof(dest_addr));
     if (ret < 0) {
@@ -44,4 +48,5 @@ int main(int argc, const char* argv[]) {
     }
 
     std::cout << "received: " << data_buffer << '\n';
+    close(socket_fd);
 }
